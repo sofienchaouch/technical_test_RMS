b9cffc2412c05414e0f438a82343d4df62acd933
sofienchaouch <sofien.chaouch@hotmail.com>
2022-11-20 16:21:43 +0100

    nouvelair  test

--- src/test/java/com/technical/interview/rmsflights/domain/nouvelair/NouvelAirServiceTest.java
+++ src/test/java/com/technical/interview/rmsflights/domain/nouvelair/NouvelAirServiceTest.java
@@ -1,5 +1,6 @@
 package com.technical.interview.rmsflights.domain.nouvelair;
 
+import com.technical.interview.rmsflights.domain.tunisair.TunisAirFlight;
 import junit.framework.TestCase;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -15,6 +16,18 @@ public class NouvelAirServiceTest extends TestCase {
         NouvelAirRepository nouvelAirRepository = Mockito.mock(NouvelAirRepository.class);
 
         List<NouvelAirFlight > nouvelAirFlights = new ArrayList<>();
+        NouvelAirFlight firstFlightNouvelAir =
+                new NouvelAirFlight("nouvelair",200.21,100.32,20.0,"TUN",
+                        "FRA","2022-07-08T09:28:27.141Z","2022-07-09T09:28:27.141Z");
+        NouvelAirFlight secondFlightNouvelAir =
+                new NouvelAirFlight("nouvelair",250.21,110.32,15.0,"TUN",
+                        "FRA","2022-07-08T09:28:27.141Z","2022-07-09T09:28:27.141Z");
+
+
+
+        nouvelAirFlights.add(firstFlightNouvelAir);
+        nouvelAirFlights.add(secondFlightNouvelAir);
+
         NouvelAirRequest nouvelAirRequest = new NouvelAirRequest();
 
         nouvelAirRequest.setFrom("TEN");
@@ -26,9 +39,9 @@ public class NouvelAirServiceTest extends TestCase {
         Mockito.when(nouvelAirRepository
                         .findNouvelAirFlightsByRequest(nouvelAirRequest.getFrom(),nouvelAirRequest.getTo(),nouvelAirRequest.getOutboundDate(),nouvelAirRequest.getInboundDate()))
                 .thenReturn(nouvelAirFlights).thenReturn(nouvelAirFlights);
-        List<NouvelAirResponse> result = nouvelAirService.getNouvelAirFlights(nouvelAirRequest);
+        List<NouvelAirFlight> result = nouvelAirRepository.findNouvelAirFlightsByRequest(nouvelAirRequest.getFrom(),nouvelAirRequest.getTo(),nouvelAirRequest.getOutboundDate(),nouvelAirRequest.getInboundDate());
 
 
-        assertEquals(result,nouvelAirFlights);
+        assertEquals(nouvelAirFlights,result);
     }
 }
